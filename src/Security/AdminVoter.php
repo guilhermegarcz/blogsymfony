28f2e3a08d7c0692a14a83c1443abf2cf2ce2658
Guilherme Garcez <guilhermegarcz@gmail.com>
2019-11-26 19:13:10 +0000

    Update code to use dependency injections instead of global variable $_SERVER

--- src/Security/AdminVoter.php
+++ src/Security/AdminVoter.php
@@ -4,6 +4,7 @@
 namespace App\Security;
 
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
@@ -11,6 +12,16 @@ use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 class AdminVoter extends Voter
 {
     const OAUTH = 'oauth2';
+
+    /**
+     * @var ParameterBagInterface
+     */
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
 
     protected function supports($attribute, $subject)
     {
@@ -22,7 +33,10 @@ class AdminVoter extends Voter
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
-        if(empty($_SERVER["GOOGLE_CLIENT_ID"]) || empty($_SERVER["GOOGLE_CLIENT_SECRET"]))
+        $google_id = $this->params->get('google_client_id');
+        $google_secret = $this->params->get('google_client_secret');
+
+        if(empty($google_id) || empty($google_secret))
             return true;
 
         $session = new Session();
